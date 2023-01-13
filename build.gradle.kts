26e61044b615a09cd3cd1b2a37c7427bb01ec6c1
Anders Jändel <sviams@gmail.com>
2023-01-13 11:21:47 +0100

    Update to Kotlin 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlin_version = "1.7.21"
+val kotlin_version = "1.8.0"
 val spek_version = "2.0.19"
 val kluent_version = "1.72"
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.0"
     application
 }
 
@@ -18,6 +18,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+    implementation("com.github.shiguruikai:combinatoricskt:1.6.0")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spek_version")
     testImplementation("org.amshove.kluent:kluent:$kluent_version")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek_version")
@@ -35,6 +36,10 @@ tasks.test {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask<*> by tasks
+
+compileKotlin.compilerOptions.freeCompilerArgs.add("-Xdebug")
 
 application {
     mainClass.set("MainKt")
